3e2b41896abaf9cbb11b1d0a29ef19c5328e96d6
ivanpn <ivanpn@outlook.com>
2021-10-24 11:05:16 -0300

    envio getElementsBy...

---
+++ fundamentos/bloco-5-js-dom-seletores/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,6 @@
+console.log(document.getElementsByClassName("title")[0].innerHTML='Exercício 5.1 - JavaScript');
+document.getElementsByTagName("main-con")[0]
+document.getElementsByTagName('p')[0].innerText='Mudei o texto padrão!';
+document.getElementsByTagName('. main-content.style')[0].innerHTML='background-color: blue';
+
+
